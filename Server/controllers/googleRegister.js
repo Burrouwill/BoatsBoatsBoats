9cc6c3c513a1acab5b24e48f36be1be533d505dd
Burrouwill <wgr.burroughs@gmail.com>
2023-10-17 16:09:47 +1300

    Removed sameSite from google

--- Server/controllers/googleRegister.js
+++ Server/controllers/googleRegister.js
@@ -85,7 +85,7 @@ const handleGoogleLogin = async (req, res) => {
     await controller.updateRefreshToken(foundUser.username, refreshToken);
 
     // Redirect with the access token as a query 
-    res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 });
     res.redirect(`/?accessToken=${accessToken}`);
 
 };
