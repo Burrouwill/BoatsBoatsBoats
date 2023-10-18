5e1f3af44da6bccdf2ca5da59b3710b50ad818e4
Jepe12 <101069131+Jepe12@users.noreply.github.com>
2023-10-18 14:52:11 +1300

    Fixed verifyRoles

--- Server/middleware/verifyRoles.js
+++ Server/middleware/verifyRoles.js
@@ -1,11 +1,23 @@
-const verifyRoles = (...allowedRoles) => {
+const verifyRoles = (...validRoles) => {
     return (req, res, next) => {
-        if (!req?.roles) return res.status(401).json({ message: 'Unauthorized' });
-        const rolesArray = [...allowedRoles];
-        const result = req.roles.map(role => rolesArray.includes(role)).find(val => val === true); // For each role passed in request, check if it is contained within array of valid roles. Find the first true, if there is one = Good, only need one
-        if (!result) return res.status(401).json({ message: 'Unauthorized' });
-        // There was a true, we allow the route to be accessed
+        console.log(validRoles);
+        console.log(req.roles);
+        if (!req?.roles || typeof req.roles !== 'object') {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const roleIDs = Object.values(req.roles);
+        const validRoleIDs = validRoles;  // Use validRoles directly
+
+        const result = roleIDs.some(roleID => validRoleIDs.includes(roleID));
+
+        if (!result) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        // There was a valid role, allowing access to the route
         next();
+
     }
 }
 
