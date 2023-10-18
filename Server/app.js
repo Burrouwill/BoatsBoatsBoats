7bc5f8bf504c315dafb4fc49ecd2a5ffbfe5da54
DarkZek <marshall@rubyplaynetwork.com>
2023-10-19 10:21:46 +1300

    Increase file size limit

--- Server/app.js
+++ Server/app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 })
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
